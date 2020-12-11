3a1ba85edfca02077b7a26cdcaf9a06acd966e33
Brennan Roel <brennanroel@gmail.com>
2020-12-11 15:01:25 -0600

    added form handler for create, working on post fetch

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const endPoint = 'http://localhost:3000/api/v1/games';
 
-document.addEventListener('DOMContentLoaded', () => {
-    // fetch and load
-    console.log("DOM is Loaded");
+document.addEventListener('DOMContentLoaded', () => {// fetch and load
     getGames()
+
+    const createGameForm = document.querySelector("#create-game-form")
+
+    createGameForm.addEventListener("submit", (e) => createFormHandler(e))
 })
 
 function getGames() {
     fetch(endPoint)
     .then(response => response.json())
     .then(games => {
-        console.log(games);
         games.data.forEach(game => {
-            // double check how your data is nested in the console so you can successfully access the attributes of each individual object
+           // data is first layer to get to attributes due to serializer
             const gameMarkup = `
               <div data-id=${game.id}>
                 <img src=${game.attributes.image_url} height="200" width="250">
@@ -24,6 +25,28 @@ function getGames() {
               <br><br>`;
     
               document.querySelector('#game-container').innerHTML += gameMarkup
-          })
+        })
+    })
+}
+
+function createFormHandler(e) {
+    e.preventdefault()
+    const titleInput = document.querySelector('#input-title').value
+    const descriptionInput = document.querySelector('#input-description').value
+    const imageInput = document.querySelector('#input-url').value
+    const genreId = parseInt(document.querySelector('#genres').value)
+    postGame(titleInput, descriptionInput, imageInput, genreId)
+}
+
+function postFetch(title, description, image_url, genre_id) {
+    fetch(endPoint, {
+    //post request
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(bodyData)
+    })
+    .then(response => response.json())
+    .then(game => {
+        
     })
 }
